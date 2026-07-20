1fe118991a348ae26eee8a0e2beb11cb32bffe3c
riversantoine93-dotcom <rivers.antoine93@gmail.com>
2026-07-20 04:19:23 -0400

    Delete components directory

--- components/ProgressBar.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProgressBar({ value }: { value: number }) {
-  return (
-    <div className="progress-wrap" aria-label={`${value}% complete`}>
-      <div className="progress-meta"><span>Course progress</span><strong>{value}%</strong></div>
-      <div className="progress-track"><div className="progress-fill" style={{ width: `${value}%` }} /></div>
-    </div>
-  );
-}
